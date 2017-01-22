1939399aeaeba9997288713f0981a881ce8da9a2
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-22 17:58:55 +0100

    Affichage du fond et du joueur

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var dodger = {
 
   preload: function () {
       //chargement image
+      game.load.image('fond', 'assets/fond.png');
+      game.load.image('player', 'assets/player.png');
   },
 
   create: function () {
       //setup du jeu
+    game.add.sprite(0, 0, 'fond');
+    this.player = game.add.sprite(300, 500, 'player').anchor.set(0.5);
   },
 
   update: function () {
