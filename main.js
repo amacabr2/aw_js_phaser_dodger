961b7ea79a654a757c369bffaaf9217e75e04efe
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-22 18:11:02 +0100

    Déplacement  joueur

--- main.js
+++ main.js
@@ -2,21 +2,45 @@ var game = new Phaser.Game(600, 600);
 
 var dodger = {
 
-  preload: function () {
-      //chargement image
-      game.load.image('fond', 'assets/fond.png');
-      game.load.image('player', 'assets/player.png');
-  },
+    /**
+     * Chargement image
+     */
+    preload: function () {
+        game.load.image('fond', 'assets/fond.png');
+        game.load.image('player', 'assets/player.png');
+    },
 
-  create: function () {
-      //setup du jeu
-    game.add.sprite(0, 0, 'fond');
-    this.player = game.add.sprite(300, 500, 'player').anchor.set(0.5);
-  },
+    /**
+     * Setup du jeu
+     */
+    create: function () {
 
-  update: function () {
-      //logique du jeu
-  }
+        //physique du jeu
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        // Affichage image
+        game.add.sprite(0, 0, 'fond');
+        this.player = game.add.sprite(300, 500, 'player');
+        this.player.anchor.set(0.5);
+
+        //Prepare le joueur
+        game.physics.arcade.enable(this.player);
+        this.cursors = game.input.keyboard.createCursorKeys();
+
+    },
+
+    /**
+     * Logique du jeu
+     */
+    update: function () {
+
+        // Déplacement joueur
+        if (this.cursors.left.isDown) this.player.body.velocity.x = -300;
+        if (this.cursors.right.isDown) this.player.body.velocity.x = 300;
+        if (this.cursors.up.isDown) this.player.body.velocity.y = -300;
+        if (this.cursors.down.isDown) this.player.body.velocity.y = 300;
+
+    }
 
 };
 
